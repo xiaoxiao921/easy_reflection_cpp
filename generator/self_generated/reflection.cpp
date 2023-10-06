09fd35bf59142bc699e3feeeb0d3fbcafd56ef0a
Quentin <xiaoxiao921@hotmail.fr>
2023-10-07 01:54:26 +0200

    Delete generator directory

--- generator/self_generated/reflection.cpp
+++
@@ -1,6 +0,0 @@
-#include "reflection.h"
-
-// clang-format off
-#include "reflected_types/config.er.cpp" //NOLINT
-#include "reflected_types/config_templates.er.cpp" //NOLINT
-// clang-format on
--- generator/self_generated/reflection.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "er/reflection/reflection.h"
-#include "er/types/all_types.h"
-
-// generated:
-#include "reflected_types/config.er.h"
-#include "reflected_types/config_templates.er.h"
